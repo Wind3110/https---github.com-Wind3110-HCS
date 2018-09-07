1c0dc0cfada47cf2109f1974f25f51938ea519d5
Wind3110 <41381286+Wind3110@users.noreply.github.com>
2018-09-08 00:37:56 +0700

    Add layout customer page

---
+++ HCS/src/app/Component/customerpage/customerregistration/customerregistration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customerregistration',
+  templateUrl: './customerregistration.component.html',
+  styleUrls: ['./customerregistration.component.css']
+})
+export class CustomerregistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
