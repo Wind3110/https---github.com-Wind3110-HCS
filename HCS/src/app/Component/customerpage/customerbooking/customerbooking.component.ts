eccf64dad35f3b41e9079d6a561e6c9bfef91974
Wind3110 <41381286+Wind3110@users.noreply.github.com>
2018-09-17 01:11:47 +0700

    update b

--- HCS/src/app/Component/customerpage/customerbooking/customerbooking.component.ts
+++ HCS/src/app/Component/customerpage/customerbooking/customerbooking.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { DlDateTimePickerDateModule } from 'angular-bootstrap-datetimepicker';
-import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { Service } from '../../../Model/ServiceModel/service.model';
 import { ServiceService } from '../../../Service/SerService/service.service';
@@ -13,8 +12,13 @@ import { ServiceService } from '../../../Service/SerService/service.service';
 import { StaffService } from '../../../Service/StaffService/staff.service'
 import { Staff } from '../../../Model/StaffModel/staff.model';
 import { Booking } from '../../../Model/BookingModel/booking.model';
-import { BookingService } from '../../../Service/BookingService/booking.sevice'
-
+import { BookingService } from '../../../Service/BookingService/booking.sevice';
+import { Time} from '../../../Model/TimeModel/time.model';
+import { DatePipe, getLocaleDateTimeFormat } from '@angular/common';
+import * as moment from 'moment';
+import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { messaging } from '../../../../../node_modules/firebase/app';
+import { formArrayNameProvider } from '../../../../../node_modules/@angular/forms/src/directives/reactive_directives/form_group_name';
 
 @Component({
   selector: 'app-customerbooking',
@@ -29,21 +33,37 @@ export class CustomerbookingComponent implements OnInit {
   serviceList: any[];
   bookList: Booking[];
   bookingForm: FormGroup;
+  timer:Time
+  date:Date;
   message:string;
 
   dropdownServiceSettings = {};
   dropdownStaffSettings = {};
 
-  constructor(config: NgbDatepickerConfig, private staffService: StaffService, private serviceSevice: ServiceService, private bookingService: BookingService, private fb: FormBuilder, private tostr: ToastrService, private modalService: NgbModal) {
+  timeFrame:string[] = ["8:00","8:30","9:00","9:30","10:00","10:30","11:00","11:30",
+                        "12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30",
+                        "16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30",
+                        "20:00","20:30","21:00"];
+  timeVal:string[] =  ["800","830","900","930","1000","1030","1100","1130",
+                        "1200","1230","1300","1330","1400","1430","1500","1530",
+                        "1600","1630","1700","1730","1800","1830","1900","1930",
+                        "2000","2030","2100"];
 
-    // Seting disable the past date
-    const currentDate = new Date();
-    config.minDate = { year: currentDate.getFullYear(), month: currentDate.getMonth() + 1, day: currentDate.getDate() };
-    // config.maxDate = {year:currentDate.getFullYear(), month: currentDate.getMonth()+1, day: currentDate.getDate()+7};
-    config.outsideDays = 'hidden';
-  }
+   time : Time[] ;
+   
+
+  constructor(config: NgbDatepickerConfig, private staffService: StaffService, private serviceSevice: ServiceService, private bookingService: BookingService, private fb: FormBuilder, private tostr: ToastrService,private datepipe: DatePipe, private modalService: NgbModal) {
+    this.updateTime();
+    // this.myFunction();
+     // Seting disable the past date
+     const currentDate = new Date();
+     config.minDate = { year: currentDate.getFullYear(), month: currentDate.getMonth() + 1, day: currentDate.getDate() };
+     // config.maxDate = {year:currentDate.getFullYear(), month: currentDate.getMonth()+1, day: currentDate.getDate()+7};
+     config.outsideDays = 'hidden';
+   }
 
   ngOnInit() {
+
 
     // Mutiple select service (ng-mutiselect-dropdown)
     this.dropdownServiceSettings = {
@@ -89,8 +109,7 @@ export class CustomerbookingComponent implements OnInit {
       this.staff.forEach(item => {
         this.staffList.push(item.FullName);
       });
-      this.staffList.push('Mặc định');
-      console.log(this.staffList)
+      // console.log(this.staffList)
     });
 
     var z = this.serviceSevice.getData();
@@ -107,6 +126,7 @@ export class CustomerbookingComponent implements OnInit {
       this.service.forEach(item => {
         this.serviceList.push(item.ServiceName);
       });
+      this.serviceList.push('Mặc định')
     });
 
     // this.bookingForm = this.fb.group({
@@ -121,13 +141,28 @@ export class CustomerbookingComponent implements OnInit {
   onSelectAll(items: any) {
     console.log(items);
   }
+  
 
   onSubmit(bookingForm: NgForm) {
+    console.log(bookingForm.value);
+    let countService:number=0;
+    let totalTime:number=30;
+    let dateStr:string =bookingForm.value.Date.toString();
+    let timeStr:string =bookingForm.value.Time.toString();
+    for (let index = 0; index < bookingForm.value.Services.length; index++) {
+      countService++;
+    }
+
+    if(countService > 1){
+      totalTime=totalTime*countService;
+      console.log(totalTime);
+      bookingForm.value.Time=this.myFunction(timeStr,totalTime);
+    }
     console.log(bookingForm.value.StaffName);
     if (bookingForm.value.StaffName === '') {
       bookingForm.value.StaffName = "Mặc định";
     }
-    console.log(bookingForm.value.StaffName);
+
     this.bookingService.insertBooking(bookingForm.value);
     this.resetForm(bookingForm);
     this.tostr.success('Đặt thành công', "Cảm ơn quý khách");
@@ -150,7 +185,46 @@ export class CustomerbookingComponent implements OnInit {
       Services: [],
       StaffName: '',
       Date: null,
-      Time: null,
+      Time: '',
     }
   }
+
+  updateTime(){
+    this.time=[];
+    for ( let index = 0; index < this.timeFrame.length; index++) {
+      this.timer={
+        TimeFrame:this.timeFrame[index].toString(),
+        TimeVal:this.timeVal[index].toString()
+      }
+      // console.log(this.time);
+      // console.log(this.timer);
+      this.time.push(this.timer);
+      this.timer=null;
+
+    }
+  }
+
+  myFunction(time:string,serviceMin:number){
+    moment.locale('vi');
+    let now=moment(time,"hmm");
+    now = now.add(serviceMin,'m')
+    return now.format("HH:mm").toString();
+   }
 }
+
+
+// let timeWorking =  ;
+// timeForm;
+// if( timeForm > timeWorking) {
+//   message = 'gio nay da co nguoi dat';
+
+// }
+// message = ''
+
+
+// let timeWorking
+// let startTime = form.value.Time;
+// let endTime = totalTime;
+// if(startTime > timeWorking ||timeWorking >endTime) {
+//   true
+// }
